7ab827859778f16d1170f1c9bdeb1f182386ebf4
Santhoshkumar-p <sanssoft12@gmail.com>
2017-01-17 21:27:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,2 @@
+var data = {name:"Santhoshkumar", age:21, birthplace:"TamilNadu", dob:"07 May 1996"};
+console.log("Name : "+ data['name'] + ", Age :  "+ data['age'] +", Place of birth : " + data['birthplace'] + " and Date of birth : " + data['dob'] ); 	
